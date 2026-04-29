9b7b619582bad60ff9733631164ba55d4da529de
PedroMRibeiro <manel.2007.ribeiro@gmail.com>
2026-04-29 12:39:44 +0100

    s

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 const authRoutes = require('./routes/auth.routes');
